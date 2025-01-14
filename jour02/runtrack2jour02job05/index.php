e30ba8f4795daf013995424b56cd3db829cada03
thomas-robert <thomas.robert@laplateforme.io>
2025-01-15 00:17:31 +0100

    job 5

--- jour02/runtrack2jour02job05/index.php
+++ jour02/runtrack2jour02job05/index.php
@@ -1,22 +1,28 @@
+
 <?php
-for ($i = 0; $i <= 100; $i++) {
-    if ($i <= 20) {
-        echo "<i>$i</i><br>";
+/**
+ * Déterminer si un nombre est premier
+ * @param int $number nombre à tester
+ * @return boolean
+ */
+function isPrime($number) {
+    //boucle de 2 au nombre à tester
+    for ($i = 2; $i < $number; $i++) {
+        //test du qutotien de la division
+        if ($number % $i == 0) {
+            return FALSE;
+        }
     }
-    elseif ($i == 42) {
-        echo "La Plateforme_<br>";
-    }
-    if ($i == 42) {
-       continue;
-    }
-    if ($i >= 25 && $i <= 50) {
-        echo "<u>$i</u><br>";
-    }
-  
-    if ($i >= 50) {
-        echo "$i<br>";
-    }
-    if ($i >= 21 && $i <= 24) {
-        echo "$i<br>";
+ 
+    //Aucun diviseur trouvé, c'est un nombre permier
+    return TRUE;
+}
+ 
+// boucle de 0 à 99
+echo 'Nombre premier de 0 à 1000: ';
+for ($i = 3; $i < 1000; $i++) {
+    if (isPrime($i)) {
+        echo $i.' ';
     }
 }
+
